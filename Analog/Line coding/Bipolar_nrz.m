a073e83498e43e0c2bf3788b916f7d4b8f633fac
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-22 12:21:42 +0600

    Line coding Bipolar NRZ

---
+++ Analog/Line coding/Bipolar_nrz.m
@@ -0,0 +1,34 @@
+clear all;
+close all;
+fs=100;
+t=0:1/fs:1;
+bit=[ 1 1 0 1 0 0 1];
+time=[];
+dig_sig=[];
+count=0;
+for i=1:1:length(bit)
+    if(bit(i)==1)
+        count=count+1;
+     if(rem(count,2)==0)
+         bit(i)=-1;
+     end
+    end
+end
+for i=1:1:length(bit)
+    dig_sig=[dig_sig (bit(i)==0)*zeros(1,length(t))+(bit(i)==1)*ones(1,length(t))+(bit(i)==-1)*-ones(1,length(t))];
+        time=[time t];
+        t=t+1;
+end
+  subplot(2,1,1);
+plot(time,dig_sig,'r','linewidth',3)
+axis([0 time(end) -1 1]);
+xlabel('time');
+ylabel('amplitude');
+title('Bipolar nrz(1 1 0 1 0 0 1)');
+grid on;
+
+
+
+
+
+        
